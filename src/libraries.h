b656bfe781dad754e14fcc14b1ea599d6be592a2
Anime Sex Storm (ASS) <puppy0caminminecraft@gmail.com>
2020-02-02 13:23:12 +1000

    move base64 to relevant submodule

--- src/libraries.h
+++ src/libraries.h
@@ -5,9 +5,9 @@
 #include <td/telegram/td_api.hpp>
 #include <td/telegram/td_api.h>
 #include <vector>
-#include "base64.h"
+#include "base64/base64.h"
 #include <type_traits>
-#include "base64.cpp"
+#include "base64/base64.cpp"
 #include <queue>
 #include <map>
 #include <td/telegram/Client.h>
